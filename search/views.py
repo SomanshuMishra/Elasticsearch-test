84ad812b4ca8b86a035c5a6d74927d98f51bcddc
Somanshu Mishra <somanshumishra@Somanshus-MacBook-Air.local>
2022-03-23 11:30:28 -0700

    1 cr indexing done

--- search/views.py
+++ search/views.py
@@ -54,34 +54,102 @@ class SearchPostImportKey(APIView):
         BOL = request.data.get('BOL',None)
         HOUSE_BILL = request.data.get('HOUSE_BILL',None)
         Country_Code = request.data.get('Country_Code',None)
-        print(BOL,HOUSE_BILL)
+        limit = int(request.data.get('limit',100))
+        page = int(request.data.get('page',1))
+        start = page*1000
+        end = start+1000
         status = True
+        all_val = {}
         if BOL:
             query=BOL
             field ='BOL'
-            print('BOL')
-        elif HOUSE_BILL:
+            all_val[field] = query 
+        if HOUSE_BILL:
             query=HOUSE_BILL
             field = 'HOUSE_BILL'
-        elif Country_Code:
+            all_val[field] = query 
+        if Country_Code:
             query=Country_Code
             field = 'Country_Code'
+            all_val[field] = query 
         else:
             query=''
             field = ''
             status = False
         if status:
-            q = Q(
+            # print(query,'query')
+            # q = Q(
+            #     # 'bool',
+            #     # "size": 10,
+            #     "query":{
+            #         "bool": {
+            #             for key ,val in all_val.items():
+            #                 "must" : {
+            #                     "match" : {key:val}
+            #                 },
+            #         }
+            #     }
+            #     must=[
+            #         Q('match', Country_Code=query),
+            #     ],
+            #     minimum_should_match=1)
+            q1 = Q(
                     'multi_match',
+                    # size = 100,
                     query=query,
                     fields = [
                         field
                     ]
                 )
-            search = self.search_document.search().query(q)
+            s = self.search_document.search().query(q1)
+            print(s.count())
+            import math
+            search = self.search_document.search().query(q1)[start:end]
             response = search.execute()
             serializer = self.serializer_class(response, many=True)
-            print(serializer.data,'data')
-            return JsonResponse(serializer.data,safe=False)
+            return JsonResponse({'total count':math.ceil(s.count()/100),'page':page,'data':serializer.data})
         else:
             return HttpResponse('No Keyword Sent')
+
+
+
+
+# tech_ids = ['qwe1', 'qwe2', 'qwe3', 'qwe4', 'qwe5', 'qwe6', 'qwe7']
+# {
+#   "query": {
+#     "bool": {
+#       "must": [
+#         {
+#           "match": {
+#             "detail": "calci"
+#           }
+#         },
+#         {
+#           "bool": {
+#             "should": 
+#               [{
+#                 "match_phrase": { "tech_id": tid }
+#               } for tid in tech_ids]
+#           }
+#         }
+#       ]
+#     }
+#   }
+# }
+
+
+
+
+# GET /_mget
+# {
+#   "docs": [
+#     {
+#       "_index": "importkey",
+#       "_id": "4870000"
+#     },
+#     {
+#       "_index": "my-index-000001",
+#       "_id": "2"
+#     }
+#   ]
+# }
